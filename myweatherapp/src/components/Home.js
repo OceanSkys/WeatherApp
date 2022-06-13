26a13b037d0b32421d99c55a37885798e9db3cb3
OceanSkys <90655578+OceanSkys@users.noreply.github.com>
2022-06-12 17:58:09 -0700

    Added google font material icons to my WeatherStats component, added a couple of function methods to temperature and time props, addded an if/else statement for some practice, and added some inline css styling

--- myweatherapp/src/components/Home.js
+++ myweatherapp/src/components/Home.js
@@ -3,20 +3,21 @@ import SearchBar from './SearchBar'
 import './Home.css';
 import WeatherStats from './WeatherStats';
 
+// const API_KEY = process.env.REACT_APP_API_KEY;
+
 export default function WeatherFetch() {
 
     const [locationKey, setLocationKey] = useState('');
     const [weather, setWeather] = useState('');
     const [show, setShow] = useState(false);
-    const [tempy, setTempy] = useState('')
     // const [data, setData] = useState('')
 
     
     const getWeather = async (locationKey) =>{
 
-        const apikey = "f2b6b296aadd41d49ee8684f217a9e16";
+        const API_KEY = 'f2b6b296aadd41d49ee8684f217a9e16';
 
-        const url = `https://api.weatherbit.io/v2.0/current?city=${locationKey}&key=${apikey}`;
+        const url = `https://api.weatherbit.io/v2.0/current?city=${locationKey}&key=${API_KEY}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
@@ -41,7 +42,7 @@ export default function WeatherFetch() {
         // instead of using weather/data is a parameter, I'm using locationkey value length as a parameter for showing results
         if (locationKey.length > 1) {
             setShow(true)
-            setTempy(weather?.data[0]?.temp * 9/5 + 32 )
+            // setTempy(weather?.data[0]?.temp * 9/5 + 32)
         } else if (typeof weather === 'undefined' || locationKey.length == 0) {       
             setShow(false)
             console.log('Search results undefined')       
@@ -67,7 +68,7 @@ export default function WeatherFetch() {
             <WeatherStats 
                 name={weather?.data[0]?.city_name}
                 state={weather?.data[0]?.state_code}
-                temperature={tempy}
+                temperature={(weather?.data[0]?.temp * 9/5 + 32)}
                 description={weather?.data[0]?.weather.description}
                 time={weather?.data[0]?.datetime}
                 />
--- myweatherapp/src/components/WeatherStats.js
+++ myweatherapp/src/components/WeatherStats.js
@@ -1,7 +1,23 @@
 
 export default function WeatherStats(props) {
 
+    var desc = 'oof';
 
+    if (props.temperature >= 70) {
+
+        var desc = 'Looks like its sunny today';
+        var weatherimg = 'sunny';
+        var face = 'sentiment_very_satisfied';
+
+    } else if (50 > props.temperature < 70) {
+        var desc = 'A little cooler today'
+        var weatherimg = 'ac_unit';
+        var face = 'sentiment_satisfied';
+    } else {
+        var desc = 'its cooooooooooold'
+        var weatherimg = 'severe_cold';
+        var face = 'mood_bad';
+    };
 
     return (
         <div>
@@ -9,9 +25,28 @@ export default function WeatherStats(props) {
                 <div className='row'>
                     <div className='box'>
                         <h1>{props.name}, {props.state}</h1>
-                        <h1>{props.temperature} °F</h1>
+                        <div className="row2">
+                            <div>
+                                <h1>{props.temperature.toFixed(1)} °F</h1>  
+                            </div>
+                            <div className='floating' style={{marginLeft: 10, marginTop: 5 }}>
+                                <span class="material-symbols-outlined"  style={{fontSize: 50}}>
+                                {weatherimg}
+                                </span>        
+                            </div>
+                        </div>   
                         <h1>{props.description}</h1>
-                        <h1>{props.time}</h1>                        
+                        <h1>{(props.time).slice(8)}</h1>  
+                        <div className="row2">
+                            <div>
+                                <h1>{desc}</h1>
+                            </div>
+                            <div style={{marginTop: 10, marginLeft: 10}}>
+                                <span class="material-symbols-outlined" style={{fontSize: 50}}>
+                                {face}
+                                </span>        
+                            </div>
+                        </div>   
                     </div> 
                 </div>
             </div>
