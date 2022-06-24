8338ceab4140dac7433cd0a878591cf63bfafb04
OceanSkys <90655578+OceanSkys@users.noreply.github.com>
2022-06-23 17:04:20 -0700

    Added api function call to SearchBar component, added a handleSubmit event to where you get the api results once you press enter or hit the submit/arrow button. Deleted api call from home component, and only left SearchBar coomponent in home component.

--- myweatherapp/src/components/Home.js
+++ myweatherapp/src/components/Home.js
@@ -1,78 +1,12 @@
-import { useEffect, useState} from 'react';
 import SearchBar from './SearchBar'
 import './Home.css';
-import WeatherStats from './WeatherStats';
-
-// const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default function WeatherFetch() {
-
-    const [locationKey, setLocationKey] = useState('');
-    const [weather, setWeather] = useState('');
-    const [show, setShow] = useState(false);
-    // const [data, setData] = useState('')
-
-    
-    const getWeather = async (locationKey) =>{
-
-        const API_KEY = 'f2b6b296aadd41d49ee8684f217a9e16';
-
-        const url = `https://api.weatherbit.io/v2.0/current?city=${locationKey}&key=${API_KEY}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-
-        if (data) {
-            setWeather(data);
-        };
-
-        // added setShow hook so WeatherStats props aren't calling empty data array for api
-        // added setTemperature/setTempy hook to change celcius to fahrenheit
-        // if (weather != 1) {
-        //     setShow(true)
-        //     setTempy(weather?.data[0]?.temp * 9/5 + 32 )
-        // } else if (typeof weather === 'undefined' || weather.length <= 0) {       
-        //     setShow(false)
-        //     console.log('Search results undefined')       
-        // }
-    
-    }
-    //Updates search results for getWeather function
-    useEffect(() => {
-        // instead of using weather/data is a parameter, I'm using locationkey value length as a parameter for showing results
-        if (locationKey.length > 1) {
-            setShow(true)
-            // setTempy(weather?.data[0]?.temp * 9/5 + 32)
-        } else if (typeof weather === 'undefined' || locationKey.length == 0) {       
-            setShow(false)
-            console.log('Search results undefined')       
-        }
-        getWeather(locationKey)
-    }, [locationKey]);
 
     return (
         <div>
             <SearchBar
-            locationKey={locationKey}
-            setLocationKey={setLocationKey}
             />
-            <div className='container'>
-                <div className='row'>
-                    <div className='box2'>
-                        <h1 className='floating'>Begin your search!</h1>                      
-                    </div> 
-                </div>
-            </div>
-           { 
-           show &&
-            <WeatherStats 
-                name={weather?.data[0]?.city_name}
-                state={weather?.data[0]?.state_code}
-                temperature={(weather?.data[0]?.temp * 9/5 + 32)}
-                description={weather?.data[0]?.weather.description}
-                time={weather?.data[0]?.datetime}
-                />
-           }
         </div>
     )
 }
--- myweatherapp/src/components/SearchBar.js
+++ myweatherapp/src/components/SearchBar.js
@@ -1,30 +1,66 @@
 import React from 'react';
+import { useState } from 'react'
+import WeatherStats from './WeatherStats';
 
-const SearchBar = (props) => {
+const SearchBar = () => {
+    const [weather, setWeather] = useState(null);
+    const [locationKey, setLocationKey] = useState('');
 
-    function handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
+        if (!locationKey) return;
+
+        async function getWeather(locationKey) {
+
+            const API_KEY = 'f2b6b296aadd41d49ee8684f217a9e16';
+    
+            const url = `https://api.weatherbit.io/v2.0/current?city=${locationKey}&key=${API_KEY}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            setWeather(data);
+            console.log(data);
+        }
+    getWeather(locationKey)
 
     }
 
     
     return (
-        <form onSubmit={handleSubmit} className='searchbar'>
-            <h1 className='citysearch'>Search Your City</h1>
-            <input
-                className='searchbarinput'
-                value={props.value}
-                onChange={(event) => props.setLocationKey(event.target.value)}
-                placeholder='ex: Los Angeles, CA '
-                type='text'       
-            >
-            </input>
-            <button className='submit' type="submit">
-                <span className="material-symbols-outlined">
-                    arrow_forward
-                </span>
-            </button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit} className='searchbar'>
+                <h1 className='citysearch'>Search Your City</h1>
+                <input
+                    className='searchbarinput'
+                    value={locationKey}
+                    onChange={(event) => setLocationKey(event.target.value)}
+                    placeholder='ex: Los Angeles, CA '
+                    type='text'       
+                >
+                </input>
+                <button className='submit' type="submit">
+                    <span className="material-symbols-outlined">
+                        arrow_forward
+                    </span>
+                </button>
+            </form>
+            <div className='container'>
+                <div className='row'>
+                    <div className='box2'>
+                        <h1 className='floating'>Begin your search!</h1>                      
+                    </div> 
+                </div>
+            </div>
+        { weather && 
+            <WeatherStats 
+                name={weather?.data[0]?.city_name}
+                state={weather?.data[0]?.state_code}
+                temperature={weather?.data[0]?.temp * 9/5 + 32}
+                description={weather?.data[0]?.weather.description}
+                time={weather?.data[0]?.datetime}
+                />
+          
+            }
+        </div>
     );
 };
 
